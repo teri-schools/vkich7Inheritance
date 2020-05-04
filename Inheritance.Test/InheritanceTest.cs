71e18aec9da5c8b16b4e79b9f0bb08e6abeffd99
Ivan Zamkovyi <izamkovyi@gmail.com>
2020-05-04 14:16:01 +0300

    Skeleton

---
+++ Inheritance.Test/InheritanceTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Inheritance.Test
+{
+    [TestFixture]
+    public class InheritanceTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
